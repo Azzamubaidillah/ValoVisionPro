9c3f250b28497dd326c193b34fb8e6b38d569e95
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-08-14 01:37:35 +0700

    Initial Commit

---
+++ ValoVision/ValoVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValoVisionApp.swift
+//  ValoVision
+//
+//  Created by Azzam Ubaidillah on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ValoVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
